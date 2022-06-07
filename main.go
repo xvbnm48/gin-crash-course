676043f520b6921cc75b1a0c4649d85aa6a6871e
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-06-07 21:55:25 +0700

    change

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -11,18 +12,21 @@ func main() {
 	var router = gin.Default()
 	var addr = ":8080"
 
-	v1 := router.Group("/api/v1")
+	// v1 := router.Group("/api/v1")
 
-	v1.GET("/hello", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"message": "Hello World",
-		})
+	// v1.GET("/hello", func(ctx *gin.Context) {
+	// 	ctx.JSON(http.StatusOK, gin.H{
+	// 		"message": "Hello World",
+	// 	})
+	// })
+	router.POST("/products/:id", func(c *gin.Context) {
+		// var id = c.Param("id")
+		var id = c.PostForm("id")
+		fmt.Println("id", id)
+		c.String(http.StatusOK, "Hello World")
 	})
-	router.GET("/get", func(c *gin.Context) {
-		c.String(200, "get")
-	})
-	v1.POST("/post", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "post")
-	})
+	// v1.POST("/post", func(ctx *gin.Context) {
+	// 	ctx.String(http.StatusOK, "post")
+	// })
 	log.Fatal(router.Run(addr))
 }
